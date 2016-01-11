cb36d52a426f2fe432285e7e09046421f15f1660
DAVID HEYDUCK <heyduckd@gmail.com>
2016-01-11 14:41:59 -0800

    math working in app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-// my javascript will be in here.
+var pikePlace = {
+  minCustomers: 14,
+  maxCustomers: 55,
+  avgCups: 1.2,
+  avgLbs: 3.7,
+
+  numCustomers: function(){
+    return Math.floor((Math.random() * (this.maxCustomers - this.minCustomers + 1)) + this.minCustomers);
+  },
+  cupsSold: function() {
+    return (this.numCustomers() * 1.2) / 20;
+  },
+  toGo: function() {
+    return (this.numCustomers() * 3.7);
+  },
+  totalLbs: function() {
+    return (this.cupsSold() + this.toGo());
+  }
+}
+
+
+
+// function totalLbs() = {
+//   console.log(numCustomers, (cupsSold + toGo));
+// }
+//
+// totalLbs();
